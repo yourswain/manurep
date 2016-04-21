0d54474f57cae8e721f94d5d42951a6c7798f1e0
manumit-mukund <manumit_mukund@yahoo.com>
2016-04-22 01:27:02 +0530

    Committing ChckAuthority.java to B3...

--- DesignPattern/src/chain/CheckAuthority.java
+++ DesignPattern/src/chain/CheckAuthority.java
@@ -14,8 +14,8 @@ class CheckAuthority {
         while (true) {
             System.out.println("Enter the amount to check who should approve your expenditure.");
             System.out.print(">");
-            System.out.print("B1");
-            System.out.print("B1");
+            System.out.print("B3");
+            System.out.print("B3");
             double d = Double.parseDouble(new BufferedReader(new InputStreamReader(System.in)).readLine());
             manager.processRequest(new PurchaseRequest(d));
         }
